e2023212794f0004991f91146f2e97686de4592a
robert-neuhaus <robert.neuhaus@my.utsa.edu>
2018-10-06 17:40:30 -0500

    Removed temp books

--- src/model/BookList.java
+++ src/model/BookList.java
@@ -10,21 +10,21 @@ public class BookList {
 	public static BookList getInstance() {
 		if (instance == null) {
 			instance = new BookList();
-			books.add(new Book(0, "The Plunger", 
-					"The Plunger is back at it again, plunging his way to the top of the charts "
-					+ "in this thrilling crime drama.", 2018, "123456780"));
-			books.add(new Book(1, "Rise of the Necromancer", 
-					"The Necromancer is back at it again, necromancing his way to the top of the charts "
-					+ "in this thrilling crime drama.", 2018, "123456781"));
-			books.add(new Book(2, "Moby Dick", 
-					"Moby Dick is back at it again, moby-dicking his way to the top of the charts "
-					+ "in this thrilling crime drama.", 1851, "123456782"));
-			books.add(new Book(3, "Pyrotechnic Life Hacks",
-					"The pyrotechnic is back at it again, pyrotechnicing his way to the top of the charts "
-					+ "in this thrilling crime drama.", 1851, "123456782"));
-			books.add(new Book(4, "Art of the Script Kiddy",
-					"The script kiddy is back at it again, script-kiddying his way to the top of the charts "
-					+ "in this thrilling crime drama.", 1851, "123456782"));
+//			books.add(new Book(0, "The Plunger", 
+//					"The Plunger is back at it again, plunging his way to the top of the charts "
+//					+ "in this thrilling crime drama.", 2018, "123456780"));
+//			books.add(new Book(1, "Rise of the Necromancer", 
+//					"The Necromancer is back at it again, necromancing his way to the top of the charts "
+//					+ "in this thrilling crime drama.", 2018, "123456781"));
+//			books.add(new Book(2, "Moby Dick", 
+//					"Moby Dick is back at it again, moby-dicking his way to the top of the charts "
+//					+ "in this thrilling crime drama.", 1851, "123456782"));
+//			books.add(new Book(3, "Pyrotechnic Life Hacks",
+//					"The pyrotechnic is back at it again, pyrotechnicing his way to the top of the charts "
+//					+ "in this thrilling crime drama.", 1851, "123456782"));
+//			books.add(new Book(4, "Art of the Script Kiddy",
+//					"The script kiddy is back at it again, script-kiddying his way to the top of the charts "
+//					+ "in this thrilling crime drama.", 1851, "123456782"));
 		}
 		return instance;
 	}
